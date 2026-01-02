848752f26b22f5aeed6354bdd1deed7f06bf849d
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-01-02 12:31:36 -0500

    Enhanced psuedocode for send.email.py

--- refactored.steps/4.send.email.py
+++ refactored.steps/4.send.email.py
@@ -1,2 +1,8 @@
-## establish connection with gmail account
-## add email file to drafts
+def create_gmail_draft(
+    email_msg,
+    attachment_path: str
+) -> str:
+    """
+    Attaches PDF and uploads draft to Gmail
+    Returns draft_id
+    """
