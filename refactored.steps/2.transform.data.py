34ead0a57792864eec4a66baa1637e9d9c09b227
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-01-08 12:57:57 -0500

    Import Libraries
    Realized .msg is proprietary to Miscrsoft/Outlook and need a different library

--- refactored.steps/2.transform.data.py
+++ refactored.steps/2.transform.data.py
@@ -1,10 +1,16 @@
+from __future__ import annotations
+
+from email.message import EmailMessage 
+from pathlib import Path
+
+
 def build_subject(invoice_number: int) -> str:
-    return f"Client Invoice No.: {invoice_number}"
+    return f"Client Invoice(s) No.: {invoice_number}"
 
 def build_body() -> str:
     return """Hello,
 
-Attached, please see the following client invoice.
+Attached, please see the following invoice.
 
 Thanks.
 
