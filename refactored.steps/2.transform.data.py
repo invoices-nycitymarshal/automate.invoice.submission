102acc2306eeb478127bcdd74c39c198baef7e26
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-01-08 14:10:12 -0500

    build_email_msg()

--- refactored.steps/2.transform.data.py
+++ refactored.steps/2.transform.data.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-
 from email.message import EmailMessage 
 from pathlib import Path
-
+from typing import Optional
+from affix.recipients import add_recipients
 
 def write_subject(invoice_number: int) -> str:
     return f"Client Invoice(s) No.: {invoice_number}"
@@ -17,3 +17,14 @@ Thanks.
 – Marshal Grossman and Bailey
 """
 
+def build_email_msg(
+        client_id: str,
+        invoice_num: int,
+        pdf_path: Path,
+        sender_email: str,
+
+) -> EmailMessage:
+    msg = EmailMessage()
+    msg["From"] = sender_email
+    msg["Subject"] = write_subject(invoice_num)
+    msg.set_content(write_body())
