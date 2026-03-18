bbed0a1c87050e563e3a7e444e73c253304fe032
Anthony Nuccio <87153552+anthony-nuccio@users.noreply.github.com>
2026-03-18 10:13:33 -0400

    dleeted outdated version attempts

--- pdf.to.eml/main.py
+++
@@ -1,9 +0,0 @@
-# decided to use argparse for parsing documents
-
-from __future__ import annotations
-import argparse
-from pathlib import Path
-
-def iterate_pdfs(input_dir: str | Path) -> list[Path]:
-        print("Hello, World!")
-
--- pdf.to.eml/refactored.steps/4.send.email.py
+++
@@ -1,8 +0,0 @@
-def create_gmail_draft(
-    email_msg,
-    attachment_path: str
-) -> str:
-    """
-    Attaches PDF and uploads draft to Gmail
-    Returns draft_id
-    """
