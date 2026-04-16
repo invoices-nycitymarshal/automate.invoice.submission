c70cfb7991f5a0a5172678bdf08894559797a8d6
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-04-16 13:33:03 -0400

    re-do config and main to accomdate letterhead

--- csv2pdf/main.py
+++ csv2pdf/main.py
@@ -1,5 +1,25 @@
-
 from split_functions.workflow import run_split_workflow
+from split_functions.add_letterhead import add_letterhead_pdf_to_folder
+from split_functions.config import (
+    OUTPUT_DIR,
+    LETTERHEAD_PDF,
+    LETTERHEAD_WIDTH,
+    LETTERHEAD_TOP_MARGIN,
+    LETTERHEAD_RECURSIVE,
+)
+
+
+def main():
+    run_split_workflow()
+
+    add_letterhead_pdf_to_folder(
+        folder_path=OUTPUT_DIR,
+        letterhead_pdf_path=LETTERHEAD_PDF,
+        recursive=LETTERHEAD_RECURSIVE,
+        width=LETTERHEAD_WIDTH,
+        top_margin=LETTERHEAD_TOP_MARGIN,
+    )
+
 
 if __name__ == "__main__":
-    run_split_workflow()
+    main()
--- csv2pdf/split_functions/config.py
+++ csv2pdf/split_functions/config.py
@@ -5,14 +5,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MASTER_PDF = BASE_DIR / "master.pdf"
 RANGES_CSV = BASE_DIR / "invoice_ranges.csv"
 OUTPUT_DIR = BASE_DIR / "split_invoices"
+LETTERHEAD_PDF = BASE_DIR / "Letterhead.pdf"
 
 REQUIRED_COLUMNS = {
     "Source File",
     "Billing Code",
     "Invoice Number",
     "Absolute Start Page",
-    "Absolute End Page"
+    "Absolute End Page",
 }
 
 CLEAR_OLD_OUTPUTS = False
 FILTER_BY_SOURCE_FILE = True
+
+LETTERHEAD_WIDTH = 260
+LETTERHEAD_TOP_MARGIN = 18
+LETTERHEAD_RECURSIVE = False
