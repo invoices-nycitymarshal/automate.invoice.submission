1639794fee2a8659a09798aef8c76c7494e6b65f
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-04-16 12:38:07 -0400

    temp deletion

--- csv2pdf/main.py
+++ csv2pdf/main.py
@@ -1,7 +1,2 @@
 
-from .split_functions.workflow import run_split_worflow
 
-
-if __name__ == "__main__":
-    run_split_worflow()
-    
--- csv2pdf/split_functions/workflow.py
+++ csv2pdf/split_functions/workflow.py
@@ -12,7 +12,7 @@ from .paths         import ensure_output_dir, clear_existing_pdfs
 from .csv_reader    import read_ranges_csv, filter_rows_for_master_pdf
 from .pdf_splitter  import split_pdf_from_rows
 
-def run_split_workflows() -> None:
+def run_split_workflow() -> None:
     
     ensure_file_exists(RANGES_CSV, "Master PDF")
     rows = read_ranges_csv(RANGES_CSV, REQUIRED_COLUMNS)
