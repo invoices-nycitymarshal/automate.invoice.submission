a4db71c1d2e6f31132c32772cc924405c3c221b3
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-04-16 12:11:00 -0400

    validators.py skeletal functions

---
+++ csv2pdf/split_functions/validators.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import pandas as pd
+
+def ensure_file_exists():
+    return 0
+
+def validate_required_columns():
+    return 0
+
+def ensure_non_empty_dataframe():
+    return 0
