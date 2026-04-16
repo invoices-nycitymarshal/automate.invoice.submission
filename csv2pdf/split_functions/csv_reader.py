7a52a9092f84a86ca691572a5271f83f670a721c
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-04-16 11:52:48 -0400

    read csv functions (null)

--- csv2pdf/split_functions/csv_reader.py
+++ csv2pdf/split_functions/csv_reader.py
@@ -1,4 +1,15 @@
+from pathlib        import Path
+from .validators    import (
+    ensure_file_exists,
+    validate_required_columns,
+    ensure_non_empty_dataframe,
+)
 
+import pandas as pd
+
+
+def read_ranges_csv():
+    return 0
 
 def filter_rows_for_master_pdf():
     return 0
