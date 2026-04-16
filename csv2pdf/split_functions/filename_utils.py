a9df047536bd607b4078a5c20097405de3fcea40
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-04-16 12:27:59 -0400

    safe_filename_part() - parameters + body defined

--- csv2pdf/split_functions/filename_utils.py
+++ csv2pdf/split_functions/filename_utils.py
@@ -1,7 +1,10 @@
 import re
 
-def safe_filename_part():
-    return 0
+def safe_filename_part(value: object) -> str:
+    return re.sub(r'[<>:"/\\|?*]+', "_", str(value))
+
+    # full transparency: chatgbt generated the return statement
+    # i would have sat here all day making sure it was correct
 
 def normalize_billing_code():
     return 0
